491e27eb5ca1b4616ded9a6ff600aa95e9117978
Amal Mzoughi <amalmzoughi1999@gmail.com>
2026-09-01 15:30:15 +0100

    second commit

---
+++ tests/Bioaware/bio.spec.ts
@@ -0,0 +1,16 @@
+import {test} from 'playwright/test';
+
+test('Basic Navigation', async({page})=> {
+  await page.goto('https://about.gitlab.com/');
+  await page.waitForTimeout(3000);
+  await page.reload();
+})
+
+
+test('Interacting with web Element on Gitlab',async({page})=>{
+  await page.goto('https://about.gitlab.com/');
+  await page.getByRole('link', {name: 'Get free trial'}).click();
+  await page.getByTestId('new-user-first-name-field').fill('Amal');
+  await page.getByTestId('new-user-last-name-field').fill('Mzoughi');
+
+})
